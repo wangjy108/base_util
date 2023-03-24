f5f89dbb7a7d76c07242138f3d333759c11ad33f
wangjy108 <root@jiayuedeMacBook-Pro.local>
2023-03-24 14:19:37 +0800

    update

--- setup.py
+++ setup.py
@@ -16,14 +16,5 @@ setup(
     author_email='wangjy108@outlook.com',
     description=('UNIPF util part'),
     url='https://github.com/wangjy108/Uni-PF/tree/main/util',
-    license=None,
-    #keywords='util',
-    #install_requires=install_requires,
-    packages=find_packages(),
-    zip_safe = False,
-    #packages=packages,
-    include_package_data=True,
-    package_data={
-        '':['*.py']
-    }
+    packages=['util'],
 )
